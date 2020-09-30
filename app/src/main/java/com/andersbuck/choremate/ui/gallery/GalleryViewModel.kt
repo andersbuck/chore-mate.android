fd9948324fde04148c922ba586105792367a579a
andersbuck <buckles.anderson@gmail.com>
2020-09-29 21:12:03 -0400

    Add initial project structure

---
+++ app/src/main/java/com/andersbuck/choremate/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.andersbuck.choremate.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Bobby"
+    }
+    val text: LiveData<String> = _text
+}
